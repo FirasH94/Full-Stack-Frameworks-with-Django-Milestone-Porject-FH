ca90ed17286d19a9c01333bd77b5b193f25103fc
FirasH94 <firas.hazim94@hotmail.com>
2020-08-20 19:38:33 +0000

    setup allauth

--- fitness_paradise/fitness_paradise/urls.py
+++ fitness_paradise/fitness_paradise/urls.py
@@ -1,4 +1,4 @@
-"""boutique_ado URL Configuration
+"""fitness_paradise URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
