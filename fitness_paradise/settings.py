fc000f2690c08971654880f6dc17df82f67c2b55
FirasH94 <firas.hazim94@hotmail.com>
2020-10-21 17:27:03 +0000

    Fixed web app layout when viewed on different screen sizes.

--- fitness_paradise/settings.py
+++ fitness_paradise/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '+f(s^v7zy5!3%c+3pj59y365*7czeono!lfj$vcqq91!0k1u)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['firas-fitness-paradise-django.herokuapp.com', 'localhost']
 
